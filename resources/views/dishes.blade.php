e5d15261e16dc4a521e58e449cf8738b695565c8
danielius <danielius.bolotinas@gmail.com>
2018-02-19 20:46:38 +0200

    VSIO

--- resources/views/dishes.blade.php
+++ resources/views/dishes.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-md-12">
       @if((Auth::check()) && (Auth::user()->is_admin == 1))
-        <a href="{{ route('createDishes') }}" class="btn btn-admin btn-big" role="button">Add Dish</a>
+      <a href="{{ route('createDishes') }}" class="btn btn-admin btn-big" role="button">Add Dish</a>
       @endif
     </div>
   </div>
@@ -24,79 +24,84 @@
 <div class="container">
   <div class="row">
     @foreach ($dishes as $dish)
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img src="{{$dish->image}}" alt="fotke">
-          <div class="caption">
-            <h3>{{$dish->name}}</h3>
-            <p>{{$dish->description}}</p>
-            <p>{{$dish->price}} &euro;</p>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+
+
+        <img src="{{$dish->image}}" alt="fotke">
+
+        <div class="text-box">
+          <p>{{$dish->description}}</p>
+          <form action="{{route('addToCart', $dish->id)}}" method="POST">
+            {!! csrf_field() !!}
+            <input type="hidden" name="token" value="{{$dish->token}}">
+            <input type="hidden" name="dish_id" value="{{$dish->id}}" >
+            <input type="hidden" name="user_id" value="" >
+            <button type="submit" class="js-add-to-cart btn btn-primary" role="button">Add to cart</button>
+            </form>
             <p>
               @if((Auth::check()) && (Auth::user()->is_admin == 1))
-                <a href="{{route('editDish', $dish->id)}}" class="btn btn-admin" role="button">Edit</a>
-                <a href="{{route('deleteDish', $dish->id)}}" class="btn btn-admin" role="button">Delete</a>
+              <a href="{{route('editDish', $dish->id)}}" class="btn btn-admin" role="button">Edit</a>
+              <a href="{{route('deleteDish', $dish->id)}}" class="btn btn-admin" role="button">Delete</a>
               @endif
-              <form action="{{route('addToCart', $dish->id)}}" method="POST">
-                {!! csrf_field() !!}
-                <input type="hidden" name="token" value="{{$dish->token}}">
-                <input type="hidden" name="dish_id" value="{{$dish->id}}" >
-                <input type="hidden" name="user_id" value="" >
-                <!-- <input type="hidden" name="name" value="{{$dish->name}}" > -->
-                <!-- <input type="hidden" name="description" value="{{$dish->description}}" > -->
-                <!-- <input type="hidden" name="price" value="{{$dish->price}}" > -->
-                <button type="submit" class="js-add-to-cart btn btn-primary" role="button">Add to cart</a>
-              </form>
             </p>
+          </div>
+
+          <div class="caption">
+            <h3>{{$dish->name}}</h3>
+
+            <p>{{$dish->price}} &euro;</p>
+
           </div>
         </div>
       </div>
-    @endforeach
+      @endforeach
+    </div>
   </div>
-</div>
 
 
-@endsection
+  @endsection
 
 
-@section('javascript')
+  @section('javascript')
 
-<script type="text/javascript">
-    $(document).ready(function() {
+  <script type="text/javascript">
+  $(document).ready(function() {
 
-        $('.js-add-to-cart').on('click', function(e){  //pasiima butona ir paleidzia funkcija "e"
+    $('.js-add-to-cart').on('click', function(e){  //pasiima butona ir paleidzia funkcija "e"
 
-            e.preventDefault(); // preventina perejima i puslapi
+    e.preventDefault(); // preventina perejima i puslapi
 
-            var form = $(this).parent(); // kintamasis "fomr" lygu submitintai formai
+    var form = $(this).parent(); // kintamasis "fomr" lygu submitintai formai
 
-            $.ajax({
-                url: form.attr('action'),
-                method: 'POST',
-                data: form.serialize(),
-                success: function(data){
+    $.ajax({
+      url: form.attr('action'),
+      method: 'POST',
+      data: form.serialize(),
+      success: function(data){
 
-                    data = JSON.parse(data); // srtinga isverciam i masyva
+        data = JSON.parse(data); // srtinga isverciam i masyva
 
 
-                    var cartTotal = $('#cart .cart-total'), // priskiriam klase
-                        cartSize = $('#cart .cart-size'), // priskiriam klasia
-                        currentPrice = cartTotal.text(),
-                        currentSize = cartSize.text(),
-                        totalPrice = (currentPrice*1) + data.price,
-                        totalSize = (currentSize*1) + 1;
+        var cartTotal = $('#cart .cart-total'), // priskiriam klase
+        cartSize = $('#cart .cart-size'), // priskiriam klasia
+        currentPrice = cartTotal.text(),
+        currentSize = cartSize.text(),
+        totalPrice = (currentPrice*1) + data.price,
+        totalSize = (currentSize*1) + 1;
 
-                        cartTotal.text(totalPrice.toFixed(2));
-                        cartSize.text(totalSize);
+        cartTotal.text(totalPrice.toFixed(2));
+        cartSize.text(totalSize);
 
-                    // console.log(data);
-                },
-                error: function(msg){
-                    console.log(msg.responseText);
-                    $('html').prepend(msg.responseText);
-                }
-            })
-        });
+        // console.log(data);
+      },
+      error: function(msg){
+        console.log(msg.responseText);
+        $('html').prepend(msg.responseText);
+      }
+    })
+  });
 
-    });
-    </script>
+});
+</script>
 @endsection
